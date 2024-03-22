37b8cc161532db1d4fd3fa1a21c4c23770ca7f0a
PhatStraw <kevindsimsjr@gmail.com>
2024-03-22 15:27:41 -0700

    connectcontract

--- app/api/tx/route.ts
+++ app/api/tx/route.ts
@@ -16,8 +16,8 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
 
   const data = encodeFunctionData({
     abi: BuyMeACoffeeABI,
-    functionName: 'buyCoffee',
-    args: [parseEther('1'), 'Coffee all day!'],
+    functionName: 'signUp',
+    args: [],
   });
 
   const txData: FrameTransactionResponse = {
@@ -27,7 +27,7 @@ async function getResponse(req: NextRequest): Promise<NextResponse | Response> {
       abi: [],
       data,
       to: BUY_MY_COFFEE_CONTRACT_ADDR,
-      value: parseEther('0.00004').toString(), // 0.00004 ETH
+      value: parseEther('0.00').toString(), // 0.00004 ETH
     },
   };
   return NextResponse.json(txData);
