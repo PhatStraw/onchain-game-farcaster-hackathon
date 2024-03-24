98f5de3f886ff5ba840f33eab4f71e6d4ba05667
PhatStraw <kevindsimsjr@gmail.com>
2024-03-23 20:54:12 -0700

    finishing up

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://onchain-game-farcaster-hackathon.vercel.app';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x3bFC54c520BD1Df962bB5Ea3E9bd5ce89d8eb3C1';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x9B0e852e86819012C1f330c158981A440bE6BB85';
