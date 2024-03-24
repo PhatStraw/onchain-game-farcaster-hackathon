21882c1e2acaa41a034311bf9512cddc4899e465
PhatStraw <kevindsimsjr@gmail.com>
2024-03-23 23:30:09 -0700

    touch up

--- generate.js
+++ generate.js
@@ -57,17 +57,46 @@ const revealAbi = {
     "type": "function"}
 const { ethers } = require('ethers');
 
-const provider = new ethers.JsonRpcProvider('https://mainnet.base.org');
 
 
-async function decodeInputData() {
-    const txHash = '0x5a23476d930ade401dc4d71aa33388cf5a741a4b9e955a29a1b34836cedf059f';
-    const receipt = await provider.getTransactionReceipt(txHash);
-    if (!receipt) throw new Error("Transaction receipt not found");
-    const boolValue = parseInt(receipt.logs[0]?.data || "", 16) === 1;
-    // Replace the Web3 initialization with ethers
-    console.log(boolValue);
+// async function decodeInputData() {
+//     const txHash = '0x5a23476d930ade401dc4d71aa33388cf5a741a4b9e955a29a1b34836cedf059f';
+//     const receipt = await provider.getTransactionReceipt(txHash);
+//     if (!receipt) throw new Error("Transaction receipt not found");
+//     const boolValue = parseInt(receipt.logs[0]?.data || "", 16) === 1;
+//     // Replace the Web3 initialization with ethers
+//     console.log(boolValue);
    
-}
+// }
 
-decodeInputData();
+// decodeInputData();
+async function sendEthToSelf() {
+    // Connect to the Ethereum network
+    const provider = new ethers.JsonRpcProvider('https://mainnet.base.org');
+
+  
+    // Create a wallet instance
+    const wallet = new ethers.Wallet("10b1b74fc62d5c0767d42e4be56cdf3d5fcc023b3d66c6d05e991cb526945013", provider);
+  
+    // Define the transaction
+    const tx = {
+      to: "0xEefC1dBDe4A65131702Ff32a12f404ef7512473b", // Sending to self
+      // Value is in wei (1 ether = 1e18 wei)
+      value: 1e15
+    };
+  
+    try {
+      console.log(`Sending 0.00001 ETH to ${wallet.address}...`);
+      // Send the transaction
+      const txResponse = await wallet.sendTransaction(tx);
+      console.log('Transaction response:', txResponse);
+  
+      // Wait for the transaction to be mined
+      const receipt = await txResponse.wait();
+      console.log('Transaction receipt:', receipt);
+    } catch (error) {
+      console.error('Error sending ETH:', error);
+    }
+  }
+  
+  sendEthToSelf();
