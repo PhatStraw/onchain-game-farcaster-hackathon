b6504b6e016c997117977b3b5f449b5057395216
PhatStraw <kevindsimsjr@gmail.com>
2024-03-23 18:28:04 -0700

    now single player casino game

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://onchain-game-farcaster-hackathon.vercel.app';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x8Ef3e139Ae44D20B25a013a7896Bfa4B45ad8518';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0x3bFC54c520BD1Df962bB5Ea3E9bd5ce89d8eb3C1';
